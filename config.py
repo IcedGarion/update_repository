118f6485bc5555a5b77a8de0612a495b175d74bc
Garion <Garion.Musetta.extern@adesso.it>
2024-05-22 09:40:39 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,19 @@
+# Repositories to pull, and their branches
+repos_branch = { "reservation-cgi": "master","resevo-api-collections": "main","resevo-apigw-service": "develop","resevo-booking-service": "develop", \
+"resevo-cast": "develop","resevo-context-lib": "develop","resevo-core-lib": "develop","resevo-db-manager": "master","resevo-devops": "master", \
+"resevo-discovery-service": "develop","resevo-domain-lib": "develop","resevo-eid-service": "develop","resevo-freightbooking-service": "develop", \
+"resevo-freightcompany-service": "develop","resevo-freightpricing-service": "develop","resevo-integration-service": "develop", \
+"resevo-messaging-lib": "develop","resevo-notification-service": "develop","resevo-parent": "develop","resevo-payment-service": "develop", \
+"resevo-piletfeed-service": "develop","resevo-port-service": "develop","resevo-pricing-service": "develop","resevo-reportprint-service": "develop", \
+"resevo-rule-lib": "develop","resevo-saga-lib": "develop","resevo-sbadmin-service": "develop","resevo-ship-service": "develop", \
+"resevo-testrunnere2e-app": "develop","resevo-testsupport-lib": "develop","resevo-user-service": "develop","resevo-voy-service": "develop", \
+"resevo-reportprint-service": "develop" }
+
+# Repositories to build, in order
+repos_order = [ "resevo-parent","resevo-domain-lib","resevo-core-lib","resevo-context-lib","resevo-messaging-lib","resevo-saga-lib","resevo-rule-lib", \
+"resevo-testsupport-lib","resevo-apigw-service","resevo-booking-service","resevo-db-manager", "resevo-discovery-service","resevo-freightbooking-service", \
+"resevo-freightcompany-service", "resevo-freightpricing-service","resevo-integration-service","resevo-notification-service","resevo-payment-service", \
+"resevo-piletfeed-service", "resevo-port-service","resevo-pricing-service","resevo-reportprint-service","resevo-sbadmin-service", \
+"resevo-ship-service", "resevo-testrunnere2e-app","resevo-user-service","resevo-voy-service", "resevo-reportprint-service" ]
+
+repo_main_dir = "C:\\Users\\iTuna\\Desktop\\ADESSO\\GNV\\RESEVO\\repository"
---
+++ update_repository.py
@@ -0,0 +1,167 @@
+'''
+    Aggiorna tutti i repository facendo git pull del branch indicato e ricompilando il progetto.
+    I comandi eseguiti sono:
+    - git stash (in caso ci siano delle modifiche locali)
+    - git checkout <branch>
+    - git pull --rebase origin <branch>
+    - mvn clean install -DskipTests
+    
+    Configurazione:
+    repos_branch: elenco dei repository da aggiornare, con relativo branch di cui fare pull
+    repos_order: indica l'ordine con cui fare la build (prima le librerie, prima le dipendenze)
+    repo_main_dir: la directory contenente tutti i repository
+    (resevo-project viene ignorato perche' non compare nella lista)
+    
+    Esecuzione:
+    python update_repository.py. 
+    Il file deve essere allo stesso livello della cartella repo_main_dir, in cui sono contenuti tutti i repository
+    python3, git e mvn installati
+    per git, serve essere autenticati ai repository (tramite ssh) altrimenti per ogni repository viene richiesta l'autenticazione manuale
+'''
+
+def main():
+    
+    # For logging
+    width = width = os.get_terminal_size().columns
+    colors = {"red": '\033[91m', "yellow": '\033[93m', "green": '\033[92m', "end": '\033[0m'}
+    error_report = []
+    times = dict()
+    git_count = 0
+    git_total = len(repos_branch)
+    mvn_count = 0
+    mvn_total = len(repos_order)
+    cmd_output = "/tmp/cmd_output.txt"
+    
+    
+    # Init
+    times.update({ "start": time.time() })
+    times.update({"git": dict(), "mvn": dict()})
+    
+    
+    # Check main directory
+    if(not os.path.isdir(os.path.join(os.getcwd(), repo_main_dir))):
+        print("ERROR: missing main directory: {}".format(os.path.join(os.getcwd(), repo_main_dir)))
+        exit(1)
+    
+    # Git pull
+    print(colors["red"] + "="*int(width-15) + " GIT PULL STEP" + colors["end"])
+    for repo_dir, branch in repos_branch.items():
+        git_count += 1
+        width = width = os.get_terminal_size().columns
+        abs_repo_path = os.path.join(repo_main_dir, repo_dir)
+        
+        # execution time init
+        times["git"].update({repo_dir: dict()})
+        git_start = time.time()
+        
+        # error missing directory
+        if(not os.path.isdir(abs_repo_path)):
+            print("{}: missing directory: {}".format(repo_dir, abs_repo_path))
+            error_report.append("{}: missing directory: {}".format(repo_dir, abs_repo_path))
+            
+            # execution time calc
+            git_end = time.time()
+            times["git"][repo_dir].update({"start": git_start})
+            times["git"][repo_dir].update({"end": git_end})
+            continue
+        
+        # Git pull abs_repo_path
+        print(colors["yellow"] + "-"*int(width-(len(repo_dir)+len(str(git_count))+len(str(git_total))+7)) + " " + repo_dir + " ({} / {})".format(git_count, git_total) + colors["end"])
+        print("> cd {}, git stash && git checkout {} && git pull --rebase origin {}".format(abs_repo_path, branch, branch))
+        
+        # EXEC COMMAND
+        exit_code = os.system("cd " + abs_repo_path + " && git stash && git checkout " + branch + " && git pull --rebase origin " + branch + ' > "' + os.getcwd() + cmd_output + '"')
+        
+        # Read output
+        output = open(os.getcwd() + cmd_output)
+        
+        # error git pull
+        if(exit_code != 0):
+            error_report.append("{}: git step error ({})".format(repo_dir, "cd " + abs_repo_path + " && git stash && git checkout " + branch + " && git pull --rebase origin " + branch))
+        
+        # execution time calc
+        git_end = time.time()
+        times["git"][repo_dir].update({"start": git_start})
+        times["git"][repo_dir].update({"end": git_end})
+        
+    
+    # Mvn install
+    print(colors["red"] + "="*int(width-18) + " MVN INSTALL STEP"+ colors["end"])
+    for repo_dir in repos_order:
+        mvn_count += 1
+        width = width = os.get_terminal_size().columns
+        abs_repo_path = os.path.join(repo_main_dir, repo_dir)
+        
+        # execution time init
+        times["mvn"].update({repo_dir: dict()})
+        mvn_start = time.time()
+        
+        # error missing directory
+        if(not os.path.isdir(abs_repo_path)):
+            print("{}: missing directory: {}".format(repo_dir, abs_repo_path))
+            error_report.append("{}: missing directory: {}".format(repo_dir, abs_repo_path))
+            
+            # execution time calc
+            mvn_end = time.time()
+            times["mvn"][repo_dir].update({"start": mvn_start})
+            times["mvn"][repo_dir].update({"end": mvn_end})
+            continue
+        
+        print(colors["yellow"] + "-"*int(width-(len(repo_dir)+len(str(mvn_count))+len(str(mvn_total))+7)) + " " + repo_dir + " ({} / {})".format(mvn_count, mvn_total) + colors["end"])
+        print("> cd {}, mvn clean install -DskipTests".format(abs_repo_path))
+        
+        # EXEC COMMAND
+        exit_code = os.system("cd " + abs_repo_path + " && mvn clean install -DskipTests" + ' > "' + os.getcwd() + cmd_output + '"')
+        
+        # Read output
+        output = open(os.getcwd() + cmd_output)
+        
+        # error mvn install
+        if(exit_code != 0):
+            error_report.append("{}: mvn install step error".format(repo_dir))
+            
+        # execution time calc
+        mvn_end = time.time()
+        times["mvn"][repo_dir].update({"start": mvn_start})
+        times["mvn"][repo_dir].update({"end": mvn_end})
+
+
+    # END
+    times.update({ "end": time.time() })
+    
+    # Error report
+    print(colors["red"] + "="*int(width-4) + " END" + colors["end"])
+    if(len(error_report) == 0):
+        print("ERROR REPORT: " + colors["green"] + "OK!!" + colors["end"])
+    else:
+        print("ERROR REPORT: " + colors["red"] + "ERRORS..." + colors["end"])
+        print('\n'.join([x for x in error_report]))
+        
+    # Execution time report
+    print("Execution time:")
+    print("Total: {:.3} seconds. git: {:.3}, mvn: {:.3}".format(times["end"] - times["start"], git_end - start, mvn_end - git_end))
+    for repo_dir, branch in repos_branch.items():
+        print("{}: git step: {:.3} seconds, mvn step: {:.3} seconds".format(repo_dir, times["git"][repo_dir]["end"] - times["git"][repo_dir]["start"],  times["mvn"][repo_dir]["end"] - times["mvn"][repo_dir]["start"]))
+    
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog='Repository update', description='Repository update')
+    parser.add_argument('-t', '--test')
+    # args = 
+    
+
+# MAIN
+if __name__ == "__main__":
+    import os, time
+    from config import *
+    import argparse
+    
+    main()
+    
+
+# TODO: 
+# quanto ci mettono le singole fasi (da mostrare alla fine) + il totale
+# per ciascun elemento della fase, sul suo nome '------' dire anche quanti ne mancano (1 / 10, 2 / 10, ...)
+# -> da testare sti 2
+
+# report finale deve dire (per ciascun projetto): se ha stashato, se hai pullato roba nuova oppure no, se la mvn install è andata o no
