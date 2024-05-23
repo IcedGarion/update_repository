d28bea64d3d1e12e85bf55adefa02304541373b6
Garion <Garion.Musetta.extern@adesso.it>
2024-05-23 10:50:22 +0200

    feat: updated configs

--- config.py
+++ config.py
@@ -1,19 +1,21 @@
-# Repositories to pull, and their branches
-repos_branch = { "reservation-cgi": "master","resevo-api-collections": "main","resevo-apigw-service": "develop","resevo-booking-service": "develop", \
-"resevo-cast": "develop","resevo-context-lib": "develop","resevo-core-lib": "develop","resevo-db-manager": "master","resevo-devops": "master", \
-"resevo-discovery-service": "develop","resevo-domain-lib": "develop","resevo-eid-service": "develop","resevo-freightbooking-service": "develop", \
-"resevo-freightcompany-service": "develop","resevo-freightpricing-service": "develop","resevo-integration-service": "develop", \
-"resevo-messaging-lib": "develop","resevo-notification-service": "develop","resevo-parent": "develop","resevo-payment-service": "develop", \
-"resevo-piletfeed-service": "develop","resevo-port-service": "develop","resevo-pricing-service": "develop","resevo-reportprint-service": "develop", \
-"resevo-rule-lib": "develop","resevo-saga-lib": "develop","resevo-sbadmin-service": "develop","resevo-ship-service": "develop", \
-"resevo-testrunnere2e-app": "develop","resevo-testsupport-lib": "develop","resevo-user-service": "develop","resevo-voy-service": "develop", \
-"resevo-reportprint-service": "develop" }
+from collections import OrderedDict
 
-# Repositories to build, in order
-repos_order = [ "resevo-parent","resevo-domain-lib","resevo-core-lib","resevo-context-lib","resevo-messaging-lib","resevo-saga-lib","resevo-rule-lib", \
-"resevo-testsupport-lib","resevo-apigw-service","resevo-booking-service","resevo-db-manager", "resevo-discovery-service","resevo-freightbooking-service", \
-"resevo-freightcompany-service", "resevo-freightpricing-service","resevo-integration-service","resevo-notification-service","resevo-payment-service", \
-"resevo-piletfeed-service", "resevo-port-service","resevo-pricing-service","resevo-reportprint-service","resevo-sbadmin-service", \
-"resevo-ship-service", "resevo-testrunnere2e-app","resevo-user-service","resevo-voy-service", "resevo-reportprint-service" ]
-
+# Main directory containing all the projects
 repo_main_dir = "C:\\Users\\iTuna\\Desktop\\ADESSO\\GNV\\RESEVO\\repository"
+
+# Repositories and their branches to git-pull + to mvn install
+repositories = OrderedDict({ \
+"resevo-parent": "develop", "resevo-domain-lib": "develop", "resevo-core-lib": "develop", "resevo-context-lib": "develop", "resevo-messaging-lib": "develop", \
+"resevo-saga-lib": "develop", "resevo-rule-lib": "develop", "resevo-testsupport-lib": "develop", "resevo-apigw-service": "develop", "resevo-booking-service": "develop", \
+"resevo-db-manager": "master", "resevo-discovery-service": "develop", "resevo-freightbooking-service": "develop", "resevo-freightcompany-service": "develop", \
+"resevo-freightpricing-service": "develop", "resevo-integration-service": "develop", "resevo-notification-service": "develop", "resevo-payment-service": "develop", \
+"resevo-piletfeed-service": "develop", "resevo-port-service": "develop","resevo-pricing-service": "develop", "resevo-reportprint-service": "develop", \
+"resevo-sbadmin-service": "develop", "resevo-ship-service": "develop", "resevo-testrunnere2e-app": "develop", "resevo-user-service": "develop","resevo-voy-service": "develop", \
+"reservation-cgi": "master","resevo-api-collections": "main", "resevo-cast": "develop","resevo-devops": "master", "resevo-eid-service": "develop"
+})
+
+# Repositories to not mvn-install (not maven projects)
+mvn_exclusions = [ "reservation-cgi", "resevo-api-collections", "resevo-cast", "resevo-devops", "resevo-eid-service", "resevo-project"]
+
+# Repositories you want to manually exclude
+blacklist = []
